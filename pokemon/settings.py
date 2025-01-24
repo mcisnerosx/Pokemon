848773876d3e9b7d72c5af52d00687a02d0b94ce
Max Cisneros <mcisnerosx@hotmail.com>
2025-01-23 22:05:38 -0500

    Added django-debug-toolbar

--- pokemon/settings.py
+++ pokemon/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -26,6 +27,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'pokemon.urls'
@@ -61,6 +63,8 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
 ]
+
+INTERNAL_IPS = ["127.0.0.1",]
 
 LANGUAGE_CODE = 'en-us'
 
